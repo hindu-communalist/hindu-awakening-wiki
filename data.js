9c107d2d78245dfb4e8ce0645c7999071185364a
hindu-communalist <hindu.communalist@gmail.com>
2025-10-10 14:10:50 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,56 +1,12 @@
 const quoteData = [
   {
-    quote: "It is not in the stars to hold our destiny but in ourselves.",
-    book: "Julius Caesar",
-    author: "William Shakespeare",
-    page: 42,
-    tags: ["Motivation", "Philosophy", "Classics", "History"],
-    comments: "A powerful reminder of personal agency.",
-  },
-  {
-    quote:
-      "All that is gold does not glitter, Not all those who wander are lost.",
-    book: "The Fellowship of the Ring",
-    author: "J.R.R. Tolkien",
-    page: 154,
-    tags: ["Fantasy", "Adventure"],
-    comments: "",
-  },
-  {
-    quote:
-      "The mystery of life isn't a problem to solve, but a reality to experience.",
-    book: "Dune",
-    author: "Frank Herbert",
-    page: 220,
-    tags: ["Sci-Fi", "Philosophy"],
-    comments: "This challenges a purely analytical approach to life.",
-  },
-  {
-    quote: "The only way to do great work is to love what you do.",
-    book: "Steve Jobs",
-    author: "Steve Jobs",
-    page: 1,
-    tags: ["Motivation", "Career"],
-    comments: "Classic advice, but timeless and true.",
-  },
-  {
-    quote:
-      "Space is big. You just won't believe how vastly, hugely, mind-bogglingly big it is.",
-    book: "The Hitchhiker's Guide to the Galaxy",
-    author: "Douglas Adams",
-    page: 1,
-    tags: ["Sci-Fi", "Humor"],
-    comments: "",
-  },
-  {
-    quote:
-      "Not all of us can do great things. But we can do small things with great love.",
-    book: "A Simple Path",
-    author: "Mother Teresa",
-    page: 50,
-    tags: ["Inspiration", "Philosophy"],
-    comments: "",
-  },
+    quote: "One of the chief concerns - if not the chief concern - of the Gospels is to 'prove' that Jesus was the Messiah as prophesied in the Jewish scriptures. There are essentially two ways in which they set about doing this, depending upon the need of the case... either to bring your story into line with the prophecy or to interpret the prophecy in such a way as to bring it into conformity with the story.",
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 28,
+    tags: ["Christianity", "Jesus"],
+    comments: "Biggest scam of the last few millenniums.",
+  }
 ];
 
 const videoData = [

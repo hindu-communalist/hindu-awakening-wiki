f07f56b1200865571491e56ef0ec458fa01e1825
John Wick <hindu.communalist@gmail.com>
2025-10-10 23:20:43 +0530

    added data

--- data.js
+++ data.js
@@ -1,15 +1,147 @@
 const quoteData = [
   {
-    quote: "One of the chief concerns - if not the chief concern - of the Gospels is to 'prove' that Jesus was the Messiah as prophesied in the Jewish scriptures. There are essentially two ways in which they set about doing this, depending upon the need of the case... either to bring your story into line with the prophecy or to interpret the prophecy in such a way as to bring it into conformity with the story.",
+    quote: `The real reason for floating the myth of virgin birth seems to be that
+“there had always been a question mark hanging over Mary’s sexual
+morality” and that “it was clearly a subject which caused the early Christians
+acute embarrassment”.`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 12,
+    tags: ["Christianity", "Jesus"],
+    comments: "",
+  },
+  {
+    quote: `Inventors of falsehood enjoy an advantage over tellers of truth, especially
+when the inventors becomepowerful and wield big guns and/or weapons
+of big propaganda. Tellers of truth are forced to discuss the fictions
+floated by the inventors of falsehood.`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 19,
+    tags: ["Christianity", "Jesus"],
+    comments: "",
+  },
+  {
+    quote: `The ifs and buts used by
+Paul in this statement go to show that for him Resurrection was the
+starting point of a story which had yet to be concocted and that to
+start with there were few buyers for this starting point.`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 19,
+    tags: ["Christianity", "Jesus"],
+    comments: `The statement is -: “Now if Christ
+is preached,” he wrote to the Corinthians in 49 AD, “asraised from the
+dead, how can some of you say that there is no resurrection of the dead?
+But if there is no resurrection of the dead, then Christ has not been
+raised; if Christ has not been raised, then our preaching is in vain
+and your faith is in vain. We are even found to be misrepresenting
+God, because we testified of God that he raised Christ, whom he did not
+raise if it is true that the dead are not raised.”`,
+  },
+  {
+    quote: `It is now well known that Mark
+16.9-20 referring to Jesus’ appearance after death and the world
+mission of Christianity, have been added at a later stage. The original
+gospel comes to an end at 16.8 in the ancient manuscripts.`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 27,
+    tags: ["Christianity", "Jesus"],
+    comments: "",
+  },
+  {
+    quote: `He discovered in a monastery the correspondence between Bishop
+Clement of Alexandria who lived at the end of the first century AD
+and a contemporary character, Theodore. It concerned a passage that
+followed immediately after Mark 10.46 which makes Jesus arrive at and
+leave Jericho. Scholars were puzzled for centuries as to what happened
+at that place, but there was no clue. The correspondence between
+Clement and Theodore contains the passage which had been censored out
+of Mark for fear of raising a scandal. The passage says that Jesus
+spent several days and nights withLazarus, both of them remaining naked.`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 27,
+    tags: ["Christianity", "Jesus"],
+    comments:
+      "Here He refers to Professor Morton Smith of the Columbia University",
+  },
+  {
+    quote: `The New English Bible version of the New Testament published
+jointly by Oxford and Cambridge universities in 1961 mentions many
+instances where passages have been inserted or taken out. The most
+significant example is that of John 8.11 which tells the story of how
+Jesus saved from being stoned a woman caught in adultery. “This
+passage, which in the most widely received editions of the New
+Testament is printed in the text of John 7.53-8.11, has no fixed place
+in our ancient manuscripts. Some of them do not contain it at all.
+Some place it after Luke 21.38, others after John 7.36 or 7.52, or
+21.24.”40 In any case, the story does not occur in any manuscript
+prior to the end of the fourth century. Scholars are now agreed that it
+is an interpolation.`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 27,
+    tags: ["Christianity", "Jesus"],
+    comments: "",
+  },
+  {
+    quote: `Similar is the case of Luke 23.34 where Jesus is
+made to cry from the cross, “Father, forgive them; they do not know what they are doing.” Incidentally, these are precisely the two statements,
+apart from the Sermon on the Mount, which the Hindu admirers of
+Jesus quote most frequently`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 27,
+    tags: ["Christianity", "Jesus"],
+    comments: "",
+  },
+  {
+    quote:
+      "One of the chief concerns - if not the chief concern - of the Gospels is to 'prove' that Jesus was the Messiah as prophesied in the Jewish scriptures. There are essentially two ways in which they set about doing this, depending upon the need of the case... either to bring your story into line with the prophecy or to interpret the prophecy in such a way as to bring it into conformity with the story.",
     book: "Jesus Christ - An artifice for aggression",
     author: "Sita Ram Goel",
     page: 28,
     tags: ["Christianity", "Jesus"],
     comments: "Biggest scam of the last few millenniums.",
-  }
+  },
+  {
+    quote: `In one case the misinterpretation of a prophecy (Zechariah 9.9)
+has created a ludicrous scene — that of Jesus riding into Jerusalem not on
+one but on two asses simultaneously! It seems that the gospel writers did
+not understand the deviceof parallelismso often employed inHebrewpoetry. Zechariah never meant that the Messiah would ride on two asses at the
+same time.`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 28,
+    tags: ["Christianity", "Jesus"],
+    comments: "",
+  },
+  {
+    quote: `In the words of Morna Hooker, “They tear passages out of context,
+use allegory or typology to give old stories new meanings, contradict the
+plain meaning of the text, find references to Christ in passages where
+the original authors never intended any, and adapt or even alter the
+wording in order to make it yield the meaning they require.”`,
+    book: "Jesus Christ - An artifice for aggression",
+    author: "Sita Ram Goel",
+    page: 28,
+    tags: ["Christianity", "Jesus"],
+    comments: "",
+  },
 ];
 
 const videoData = [
-  { title: "रायगड दर्शन (भाग २) - इतिहासतज्ञ श्री. अप्पा परबांसोबत", videoId: "KXZZrolKsD0", tags: ["History"] },
-  { title: "History: The truth and lies| Gajanan Mehendale| इतिहासातील सत्य व असत्य| गजानन मेहेंदळे", videoId: "GqhkOvysMm8", tags: ["History", "Distortions"] }
+  {
+    title: "रायगड दर्शन (भाग २) - इतिहासतज्ञ श्री. अप्पा परबांसोबत",
+    videoId: "KXZZrolKsD0",
+    tags: ["History"],
+  },
+  {
+    title:
+      "History: The truth and lies| Gajanan Mehendale| इतिहासातील सत्य व असत्य| गजानन मेहेंदळे",
+    videoId: "GqhkOvysMm8",
+    tags: ["History", "Distortions"],
+  },
 ];

ff64b8ab0f33803b67d5f61116daf7bdcb7e5457
hindu-communalist <hindu.communalist@gmail.com>
2025-10-10 13:56:44 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,73 @@
+const quoteData = [
+  {
+    quote: "It is not in the stars to hold our destiny but in ourselves.",
+    book: "Julius Caesar",
+    author: "William Shakespeare",
+    page: 42,
+    tags: ["Motivation", "Philosophy", "Classics", "History"],
+    comments: "A powerful reminder of personal agency.",
+  },
+  {
+    quote:
+      "All that is gold does not glitter, Not all those who wander are lost.",
+    book: "The Fellowship of the Ring",
+    author: "J.R.R. Tolkien",
+    page: 154,
+    tags: ["Fantasy", "Adventure"],
+    comments: "",
+  },
+  {
+    quote:
+      "The mystery of life isn't a problem to solve, but a reality to experience.",
+    book: "Dune",
+    author: "Frank Herbert",
+    page: 220,
+    tags: ["Sci-Fi", "Philosophy"],
+    comments: "This challenges a purely analytical approach to life.",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    book: "Steve Jobs",
+    author: "Steve Jobs",
+    page: 1,
+    tags: ["Motivation", "Career"],
+    comments: "Classic advice, but timeless and true.",
+  },
+  {
+    quote:
+      "Space is big. You just won't believe how vastly, hugely, mind-bogglingly big it is.",
+    book: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    page: 1,
+    tags: ["Sci-Fi", "Humor"],
+    comments: "",
+  },
+  {
+    quote:
+      "Not all of us can do great things. But we can do small things with great love.",
+    book: "A Simple Path",
+    author: "Mother Teresa",
+    page: 50,
+    tags: ["Inspiration", "Philosophy"],
+    comments: "",
+  },
+];
+
+const videoData = [
+  { title: "A Tour of the Universe", videoId: "p_dDeaW_a5c", tags: ["Science", "Space"] },
+  {
+    title: "The Rise and Fall of the Roman Empire",
+    videoId: "o_R-K1A8s_A",
+    tags: ["History", "Culture"],
+  },
+  {
+    title: "The Pomodoro Technique for Productivity",
+    videoId: "mNBmG24djoY",
+    tags: ["Productivity", "Career"],
+  },
+  {
+    title: "Introduction to Neural Networks",
+    videoId: "aircAruvnKk",
+    tags: ["Science", "Technology"],
+  },
+];
